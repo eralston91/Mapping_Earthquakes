3dc38fa44b750dacaa8b04ee236822a76bf4d9fc
eralston91 <67438203+eralston91@users.noreply.github.com>
2020-10-05 13:03:45 -0700

    Add files via upload

---
+++ Mapping_Lines/static/js/logic.js
@@ -0,0 +1,56 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+
+// Create the map object with a center and zoom level.
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+// Coordinates for each point to be used in the line.
+// Coordinates for each point to be used in the polyline.
+
+
+// Create the map object with a center and zoom level.
+//let map = L.map("mapid", {
+//    center: [
+//      40.7, -94.5
+//    ],
+//    zoom: 4
+//  });
+
+
+//  Add a marker to the map for Los Angeles, California.
+// Get data from cities.js
+let cityData = cities;
+
+
+
+// We create the tile layer that will be the background of our map.
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
